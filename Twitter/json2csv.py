fc8e3e15e799a0ed2880e0d66f12c85d679ae68a
Tobias Graeve <tobias.graeve@gmail.com>
2016-06-03 01:34:48 +0200

    realised that previous search results are not real json format. so actually no chance to parse to csv.

--- Twitter/json2csv.py
+++ Twitter/json2csv.py
@@ -1,17 +1,18 @@
 import json
 import csv
 
-jsonFile = open('data/homophobie.json','r')
-jsonData = json.loads(jsonFile)
+with open('data/homophobie.json') as jsonFile:
+	jsonData = json.loads(jsonFile)
 
-csvFile = open('test.csv','a')
-csvWriter = csv.writer(csvFile)
+# csvFile = open('test.csv','a')
+# csvWriter = csv.writer(csvFile)
 
-print jsonData
+print "nothing"
 
-# for line in jsonData:
-# 	csvWriter.writerow([line.get('created_at'),
-# 						line.get('text').encode('utf-8'),
-# 						line.get('user').get('screen_name')
-# 						])
+# for item in jsonData:
+# 	print item
+	# csvWriter.writerow([line.get('created_at'),
+	# 					line.get('text').encode('utf-8'),
+	# 					line.get('user').get('screen_name')
+	# 					])
 csvFile.close()
