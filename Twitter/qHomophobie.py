23d319e4ecc0d56134475a9a77ef211647255b90
Tobias Graeve <tobias.graeve@gmail.com>
2016-06-03 02:17:09 +0200

    no idea why i have to push before revert old commits?

--- Twitter/qHomophobie.py
+++ Twitter/qHomophobie.py
@@ -1,5 +1,4 @@
 import tweepy
-from tweepy.parsers import JSONParser
 import json
 
 consumer_key = "2OM8NeaYfQRcXng9tPgFKft3t"
@@ -10,12 +9,13 @@ access_secret = "q0sAC1WtPhHt21ChPtUSrkWQqRXo4WHcmkgMtUSKBdiQU"
 auth = tweepy.AppAuthHandler(consumer_key,consumer_secret)
 # auth.set_access_token(access_token,access_secret)
 
-api = tweepy.API(auth, parser=JSONParser(), wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=True)
 
 query = "schwuchtel"
 
 count = 1
 
+<<<<<<< HEAD
 for status in tweepy.Cursor(api.search,q=query,geocode="51.1656910,10.4515260,454km", count=1).items():
 	print json.dumps(status)
 
@@ -26,3 +26,11 @@ for status in tweepy.Cursor(api.search,q=query,geocode="51.1656910,10.4515260,45
 # 		f.write(json.dumps(tweet._json))
 # 		f.write("\n")
 # 	count +=1
+=======
+for tweet in tweepy.Cursor(api.search,q=query,geocode="51.1656910,10.4515260,454km").items():
+	print ("Nummer: %5d, Inhalt: %150s" % (count,tweet.text))
+	with open('homophobie.json','a') as f:
+		f.write(json.dumps(tweet._json))
+		f.write("\n")
+	count +=1
+>>>>>>> parent of b47b0f3... trying to use JSONParser to receive search results but have  trouble with cursors then.
