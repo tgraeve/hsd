e5a1364847e6c22ba009908d947a52efa23d07a5
Tobias Graeve <tobias.graeve@gmail.com>
2016-07-12 15:54:19 +0200

    modified reading/writing due to problems in crawled data.

--- twitter/json2csv.py
+++ twitter/json2csv.py
@@ -1,19 +1,16 @@
 import json
 import csv
 
-with open('data/homophobie.json') as jsonFile:
-	jsonStr = jsonFile.read()
-	jsonData = json.loads(jsonStr)
-
 csvFile = open('data/homophobie.csv','a')
 csvWriter = csv.writer(csvFile)
 
-print "nothing"
-
-for item in jsonData:
-	print item
-	csvWriter.writerow([item.get('created_at'),
-						item.get('text').encode('utf-8'),
-						item.get('user').get('screen_name')
-						])
+with open('data/homophobie.json') as jsonFile:
+	for line in jsonFile:
+		tweet = json.loads(line)
+		csvWriter.writerow([tweet['id'],
+							tweet['user']['location'].encode('utf-8')
+							])
 csvFile.close()
+
+
+	
