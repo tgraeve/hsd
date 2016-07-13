fbe6828e411b4b3aeb6e486342c82207aa54fa0f
Tobias Graeve <tobias.graeve@gmail.com>
2016-07-13 17:37:53 +0200

    changed output to geolocation of tweets for control purposes.

--- twitter/exJson2Cons.py
+++ twitter/exJson2Cons.py
@@ -3,6 +3,11 @@ import json
 with open('data/fluechtlinge.json', 'r') as f:
 	for line in f:
 		tweet = json.loads(line)
-		print(tweet['user']['location'])
-		#if(tweet['coordinates'] is not None):
-			#print(tweet['coordinates'])
+		if(tweet['coordinates'] is not None): 			#prints coordinates of tweet
+			print(tweet['coordinates'])
+		elif(tweet['place'] is not None): 				#prints place of tweet
+			print(tweet['place']['full_name'])
+		elif(tweet['user']['location'] is not None):	#prints location of user
+			print(tweet['user']['location'])
+		else:
+			print("-*-TWEET IS NOT GEOLOCATED-*-")
