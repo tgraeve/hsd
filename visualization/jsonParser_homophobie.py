3a166d0d9389fa539c9c02ff7b3dfaa26ddc0af1
chermanns <chris-hermanns@gmx.de>
2016-08-15 18:18:12 +0200

    fixed writer for homophobie

--- visualization/jsonParser_homophobie.py
+++ visualization/jsonParser_homophobie.py
@@ -9,22 +9,25 @@ with open("json/homophobie.json", 'r') as f:
 				for line in f:
 					tweet = json.loads(line)
 					if (tweet['coordinates'] is not None):
-						print ("Koordinaten: " + tweet['coordinates'])
-						#tweetCoords =  tweet['coordinates']
-						#writer.writerow([tweetCoords])
+						print ("Koordinaten: " + str(tweet['coordinates']))
+						data = str(tweet['coordinates'])[37:-2]
+						strArray = data.split(",")
+						latlngStr = strArray[1][1:] + ", " + strArray[0]
+						#print latlngStr
+						writer.writerow([latlngStr])
 					elif (tweet['place'] is not None):
-						print ("Tweet-Place: " + tweet['place']['full_name'])
-						#g = geocoder.google(tweet['place']['full_name'])
-						#data = str(g.latlng)
-						#if data not in (None, "[]"):
-						#	writer.writerow([data[1:-1]])
+						#print ("Tweet-Place: " + tweet['place']['full_name'])
+						g = geocoder.google(tweet['place']['full_name'])
+						data2 = str(g.latlng)
+						if data2 not in (None, "[]"):
+							writer.writerow([data2[1:-1]])
 					elif (tweet['user']['location'] is not None):
-						print ("User-Location: " + tweet['user']['location'])
-						#g2 = geocoder.google(tweet['user']['location'])
-						#data2 = str(g2.latlng)
-						#if data2 not in (None, "[]"):
-						#	print (data2)
-						#	writer.writerow([data2[1:-1]])
+						print ("User-Location: " + tweet['user']['location'].encode('utf-8'))
+						g2 = geocoder.google(tweet['user']['location'])
+						data3 = str(g2.latlng)
+						if data3 not in (None, "[]"):
+							#print (data3)
+							writer.writerow([data3[1:-1]])
 
 
 print "---------- jsonParser_homophobie.py FINISHED ----------"
