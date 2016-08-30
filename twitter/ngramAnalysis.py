8bc579fbfcb18a824b3ed3c258e4d735bbb36628
Tobias Graeve <tobias.graeve@gmail.com>
2016-08-30 15:34:31 +0200

    did some testing in of tagging with this file.

---
+++ twitter/ngramAnalysis.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+from nlptools import TreeTagger
+from nlptools import ngrams
+
+tagger = TreeTagger()
+tags = tagger.tagJson("data/fluechtlinge.json")
+print tags.most_common()
