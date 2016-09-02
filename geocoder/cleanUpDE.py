5fcea4ba0ddabcb72a8db6d0929d2c618c3c8ded
chermanns <chris-hermanns@gmx.de>
2016-09-02 14:58:23 +0200

    cleaned and sorted DE.tab

--- geocoder/cleanUpDE.py
+++ geocoder/cleanUpDE.py
@@ -6,11 +6,18 @@ ifile = open('db/DE.tab', "r")
 reader = csv.reader(ifile, delimiter='\t')
 
 ofile = open('DE_cleanedUp.tab', "wb")
-writer = csv.writer(ofile, delimiter='\n', quoting= csv.QUOTE_NONE)
+writer = csv.writer(ofile, delimiter='\t', quoting= csv.QUOTE_NONE)
+
+unsortedList = []
 
 for row in reader:
-	if (row[4] not in "" and row[5] not in "" and row[9] not in "" and row[12] not in ("", "Gemeinde", "Ortsteil")):
-		newRow = row[3].lower() + "\t" + row[4] + "\t" + row[5] + "\t" + row[9] + "\t" + row[12]
-		writer.writerow([newRow])
+	if (row[4] not in "" and row[5] not in "" and row[9] not in ("", "einwohner") and row[12] not in ("", "Gemeinde", "Ortsteil", "Amt")):
+		newList = [row[3].lower(), row[4], row[5], int(row[9]), row[12]]
+		unsortedList.append(newList)
+
+sortedList = sorted(unsortedList, key=lambda x: -x[3])
+
+for i in sortedList:
+	writer.writerow(i)
 
 print "---cleanUpDE.py FINISHED---"
