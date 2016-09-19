66c71cea7308db9f5c63fb35bf0f61972d6cf918
Tobias Graeve <tobias.graeve@gmail.com>
2016-09-19 16:59:03 +0200

    get index to actual mockup stage. nearly all functionality.

---
+++ twitter/qTest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# This Python file uses the following encoding: utf-8
+import tweepy
+import json
+import os
+
+consumer_key = "2OM8NeaYfQRcXng9tPgFKft3t"
+consumer_secret = "7QP0bVI0qDPotmHG0DSI8FBJVQExCPmdJczdGQFGtC0g98zZEn"
+access_token = "67152181-hXGXzZcsCIjxtcQPWmyJjzEvm4tEmlbbu8eUivbvB"
+access_secret = "q0sAC1WtPhHt21ChPtUSrkWQqRXo4WHcmkgMtUSKBdiQU"
+
+auth = tweepy.AppAuthHandler(consumer_key,consumer_secret)
+# auth.set_access_token(access_token,access_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+query = ""
+
+count = 0
+
+print os.getcwd()
+
+for tweet in tweepy.Cursor(api.search,geocode="51.1656910,10.4515260,454km",lang="de").items():
+	with open('data/normalize.json','a') as f:
+		json.dump(tweet._json, f)
+		f.write("\n")
+	count +=1
+
+print ("Crawled %5d Tweets" % (count))
